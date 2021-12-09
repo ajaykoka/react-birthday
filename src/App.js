2c1dddad27e67210cb80ee81d8068663b7ac1d60
ajaykoka <ajaykoka1704@gmail.com>
2021-12-09 17:52:27 +0530

    Birthday with Build

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {useState} from 'react'
+import data from './data'
+import List from './components/List'
+
+function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+     <section className="container">
+       <h3> {people.length} birthdays today</h3>
+       <List people={people} />
+       <button onClick={() => setPeople([])}>Clear ALL</button>
+     </section> 
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line
+export default [
+    {
+        id:1,
+        name: 'John',
+        age: 21,
+        image: 'https://static01.nyt.com/images/2021/05/25/multimedia/25xp-johncena/25xp-johncena-mobileMasterAt3x.jpg'
+    },
+    {
+        id:2,
+        name: 'Vin Diesel',
+        age: 27,
+        image: 'https://m.media-amazon.com/images/M/MV5BMjExNzA4MDYxN15BMl5BanBnXkFtZTcwOTI1MDAxOQ@@._V1_.jpg'
+    },
+    {
+        id:3,
+        name: 'Paul Walker',
+        age: 25,
+        image: 'https://media1.popsugar-assets.com/files/thumbor/kL29MFiN7kf3VN3Rbi18s38HXMY/186x52:2258x2124/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/04/08/705/n/1922398/1c0039b55e8df427cd8537.59120730_/i/Paul-Walker.jpg'
+    },
+    {
+        id:4,
+        name: 'Dwayne',
+        age: 30,
+        image: 'https://media.self.com/photos/5f510c02d1045cb89042b8cc/4:3/w_2560%2Cc_limit/GettyImages-1219099339.jpg'
+    },
+    {
+        id:5,
+        name: 'Jason',
+        age: 30,
+        image: 'https://in.bmscdn.com/iedb/artist/images/website/poster/large/jason-statham-935-24-03-2017-13-50-52.jpg'
+    }
+]
